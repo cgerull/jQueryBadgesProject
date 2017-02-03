59df4a08117ef42e3763c27018e5a66ce90919cf
Claus Gerull <cgerull@gmail.com>
2017-02-03 23:52:07 +0100

    Add courses.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2565397.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(function(course) {
+        var courseDiv = $("<div class='course'><h3>" + course.title + "</h3><img src=" + course.badge + " alt=" + course.title + "></img><br><a class='btn btn-primary' target='_blank' href=" + course.url + ">See course</a></div>");
+        $('#badges').append(courseDiv);
+        //$('#course').append("<h3 class='course'>" + course.title + "</h3><img class='course' src=" + course.badge + "></img>");
+        console.log("Course: " + JSON.stringify(course));
+      });
+    }
+  });
 });
